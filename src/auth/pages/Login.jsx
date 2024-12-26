1001dad801f5f4c6b2bdc6b5d0c563a0e15ae0f4
Sameeha T <sameehasirajt@gmail.com>
2024-12-26 14:51:40 +0530

    '

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -55,7 +55,7 @@ const Login =() => {
                     </NavLink>
 
                     <button 
-                        className="bg-orange-500 hover:bg-orange-600 mt-5 text-white 
+                        className="bg-orange-400 hover:bg-orange-500 mt-5 text-white 
                         py-2 px-16 rounded-full font-semibold ml-[50px]"
                     >
                         Login
